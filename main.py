e1ccaa8c49783b14ffe6d4174f9797e89a95fa33
Joshua Vinson <josh.daniel.vinson@gmail.com>
2021-06-15 14:51:57 +1000

    changed to a usable concept for demo purposes

--- main.py
+++ main.py
@@ -9,27 +9,30 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 class Namespace(Enum):
-	ITEM = 1
-	PROPERTY = 2
+    ITEM = 1
+    PROPERTY = 2
 
 namespace_ids = {
-	Namespace.ITEM : '0', 
-	Namespace.PROPERTY : '120'
-	}
+    Namespace.ITEM : '0', 
+    Namespace.PROPERTY : '120'
+    }
 
 class Property(Enum):
-	INSTANCE_OF = 1
-	SUBCLASS_OF = 2
-	STUDIED_BY = 3
-	HAS_QUALITY = 4
-	HAS_PART = 5
+    INSTANCE_OF = 1
+    SUBCLASS_OF = 2
+    STUDIED_BY = 3
+    HAS_QUALITY = 4
+    HAS_PART = 5
+    PART_OF = 6
+    USES = 7
 
 property_ids = {
-	Property.INSTANCE_OF : 'P31',
-	Property.SUBCLASS_OF : 'P279',
-	Property.STUDIED_BY : 'P2579',
-	Property.HAS_QUALITY : 'P1552',
-	Property.HAS_PART : 'P527'
+    Property.INSTANCE_OF : 'P31',
+    Property.SUBCLASS_OF : 'P279',
+    Property.STUDIED_BY : 'P2579',
+    Property.HAS_QUALITY : 'P1552',
+    Property.HAS_PART : 'P527',
+    Property.PART_OF : ''
 
 }
 
@@ -47,47 +50,47 @@ def jprint(obj):
 # 	return get_value('wikibase_item', response.json())
 
 def get_value(target_key, json):
-	'''
-	returns the first value of target_key in a json
-	'''
-	value = None
-	if type(json) == dict:
-		for key in json:
-			if key == target_key:
-				return json[key]
-			else:
-				value = get_value(target_key, json[key])
-	elif type(json) == list:
-		for ele in json:
-			value = get_value(target_key, ele)
-			if value is not None:
-				break
-		
-	return value
+    '''
+    returns the first value of target_key in a json
+    '''
+    value = None
+    if type(json) == dict:
+        for key in json:
+            if key == target_key:
+                return json[key]
+            else:
+                value = get_value(target_key, json[key])
+    elif type(json) == list:
+        for ele in json:
+            value = get_value(target_key, ele)
+            if value is not None:
+                break
+        
+    return value
 
 def get_wiki_id(namespace, page_name):
-	'''
-	gets the FIRST ID for a search with page_name in namespace
-	'''
-	response = requests.get(f"https://www.wikidata.org/w/api.php?action=query&list=search&srsearch={page_name}&srnamespace={namespace_ids[namespace]}&format=json")
-	
-	if namespace == Namespace.ITEM:
-		id_ = get_value('title', response.json())
-	elif namespace == Namespace.PROPERTY:
-		id_ = get_value('title', response.json()).split(":")[1]
+    '''
+    gets the FIRST ID for a search with page_name in namespace
+    '''
+    response = requests.get(f"https://www.wikidata.org/w/api.php?action=query&list=search&srsearch={page_name}&srnamespace={namespace_ids[namespace]}&format=json")
+    
+    if namespace == Namespace.ITEM:
+        id_ = get_value('title', response.json())
+    elif namespace == Namespace.PROPERTY:
+        id_ = get_value('title', response.json()).split(":")[1]
 
-	return id_
+    return id_
 
 def get_properties(sparql, item, property_):
     sparql.setQuery(f"""
-	SELECT ?prop ?propLabel
-	WHERE
-	{{
-		wd:{item} wdt:{property_} ?prop.
-		SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
-	}}
-	
-	""")
+    SELECT ?prop ?propLabel
+    WHERE
+    {{
+        wd:{item} wdt:{property_} ?prop.
+        SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
+    }}
+    
+    """)
 
     results = sparql.query().convert()
 
@@ -122,8 +125,8 @@ sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 sparql.setReturnFormat(JSON)
 
 root_node = 'computer'
-property_type = Property.HAS_PART
-depth = 4
+property_type = Property.HAS_QUALITY
+depth = 3
 
 g = nx.Graph()
 g.add_node(root_node)
